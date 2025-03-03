a62c4c4ad5eed70df8f52832b1849e5c621b7f7c
guilherme-or <guioliroc@gmail.com>
2025-03-03 17:00:54 -0300

    feat(naming): move repository file with CRUDRepository interface from pkg/util/repository.go to pkg/domain/crud_repository.go for external use

--- pkg/domain/crud_repository.go
+++ pkg/domain/crud_repository.go
@@ -1,9 +1,8 @@
-package util
+package domain
 
 type CRUDRepository[T any] interface {
 	FindAll() ([]T, error)
 	FindById(id int) (T, error)
 	Create(entity T) (T, error)
-	Update(id int, entity T) error
 	Delete(id int) error
 }
