3c17b5df7ac14c1f83247246b40094976746424d
guilherme-or <guioliroc@gmail.com>
2025-03-03 01:10:55 -0300

    chore: rename commom variables to especific entities mapping to avoid redeclaration

--- internal/domain/entity/artist_test.go
+++ internal/domain/entity/artist_test.go
@@ -8,31 +8,31 @@ import (
 )
 
 var (
-	id          = 1
-	name        = "John Doe"
-	birthDate   = time.Date(1990, time.January, 1, 0, 0, 0, 0, time.UTC)
-	nationality = "American"
+	artistId          = 1
+	artistName        = "John Doe"
+	artistBirthDate   = time.Date(1990, time.January, 1, 0, 0, 0, 0, time.UTC)
+	artistNationality = "American"
 )
 
 // NewArtist function should return an pointer to a Artist struct
 // with the given ID, name, birthDate and nationality.
 func TestNewArtist(t *testing.T) {
-	artist := entity.NewArtist(id, name, birthDate, nationality)
+	artist := entity.NewArtist(artistId, artistName, artistBirthDate, artistNationality)
 
 	if artist == nil {
 		t.Fatal("expected Artist instance, got nil")
 	}
-	if artist.ID != id {
-		t.Errorf("expected ID %d, got %d", id, artist.ID)
+	if artist.ID != artistId {
+		t.Errorf("expected ID %d, got %d", artistId, artist.ID)
 	}
-	if artist.Name != name {
-		t.Errorf("expected Name %s, got %s", name, artist.Name)
+	if artist.Name != artistName {
+		t.Errorf("expected Name %s, got %s", artistName, artist.Name)
 	}
-	if artist.BirthDate != birthDate {
-		t.Errorf("expected BirthDate %v, got %v", birthDate, artist.BirthDate)
+	if artist.BirthDate != artistBirthDate {
+		t.Errorf("expected BirthDate %v, got %v", artistBirthDate, artist.BirthDate)
 	}
-	if artist.Nationality != nationality {
-		t.Errorf("expected Nationality %s, got %s", nationality, artist.Nationality)
+	if artist.Nationality != artistNationality {
+		t.Errorf("expected Nationality %s, got %s", artistNationality, artist.Nationality)
 	}
 	if artist.Albums != nil {
 		t.Errorf("expected Albums to be nil, got %v", artist.Albums)
