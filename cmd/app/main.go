1847e944c017f9aedfd258a4782c0d44d560e928
guilherme-or <guioliroc@gmail.com>
2025-03-03 02:03:57 -0300

    feat: app command main file config and database instanciation

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,4 +2,27 @@
 // This file is used to build and run the entire application
 package main
 
-func main() {}
+import (
+	"database/sql"
+	"github.com/guilherme-or/go-cleanarch-starter/internal/infra/database"
+
+	"github.com/guilherme-or/go-cleanarch-starter/internal/config"
+)
+
+func main() {
+	// Load environment variables
+	config.LoadEnv()
+
+	// Connect to the database and close it when at its end
+	db := databaseConn()
+	defer db.Close()
+}
+
+// Open a new database connection
+func databaseConn() *sql.DB {
+	db, err := database.PostgreSQLConn()
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
