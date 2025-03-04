b05fac507449e72fd832cf047bf676254804aa63
guilherme-or <guioliroc@gmail.com>
2025-03-04 01:37:56 -0300

    feat(setup): update main app command entrypoint to accept dependency instantiation and small testing for artist usecases

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,31 +4,65 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+
 	"github.com/guilherme-or/go-cleanarch-starter/internal/infra/database"
+	database_repository "github.com/guilherme-or/go-cleanarch-starter/internal/infra/database/repository"
+	artist_usecase "github.com/guilherme-or/go-cleanarch-starter/internal/usecase/artist"
 
 	"github.com/guilherme-or/go-cleanarch-starter/internal/infra/config"
 )
 
 func main() {
-	// Load environment variables
-	configLoad()
+	// Load environment
+	cfg()
 
-	// Connect to the database and close it when at its end
-	db := databaseConn()
+	// Establish database connection
+	db := db()
 	defer db.Close()
+
+	// Instantiate dependencies
+	deps(db)
 }
 
-func configLoad() {
+// Loads environment configuration variables
+func cfg() {
 	if err := config.LoadEnv(); err != nil {
 		panic(err)
 	}
 }
 
 // Open a new database connection
-func databaseConn() *sql.DB {
+func db() *sql.DB {
 	db, err := database.PostgreSQLConn()
 	if err != nil {
 		panic(err)
 	}
 	return db
+}
+
+func deps(db *sql.DB) {
+	// Repositories
+	artistRepo := database_repository.NewPostgreSQLArtistRepository(db)
+	// albumRepo := database_repository.NewPostgreSQLAlbumRepository(db)
+	// songRepo := database_repository.NewPostgreSQLSongRepository(db)
+
+	// Use Cases
+	// Artist
+	findAllArtistsUC := artist_usecase.NewFindAllArtistsUsecase(artistRepo)
+	findArtistByNameUC := artist_usecase.NewFindArtistByNameUsecase(artistRepo)
+	findArtistsByNationalityUC := artist_usecase.NewFindArtistsByNationalityUsecase(artistRepo)
+
+	as, _ := findAllArtistsUC.Execute()
+	j, _ := json.MarshalIndent(as, "", "  ")
+	fmt.Println(string(j))
+
+	a, _ := findArtistByNameUC.Execute("Daft Punk")
+	j, _ = json.MarshalIndent(a, "", "  ")
+	fmt.Println(string(j))
+
+	as, _ = findArtistsByNationalityUC.Execute("American")
+	j, _ = json.MarshalIndent(as, "", "  ")
+	fmt.Println(string(j))
 }
